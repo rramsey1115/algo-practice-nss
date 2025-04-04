28f71246e1c65d4db6e4e2f42ee64a032f1f5844
Ryan Ramsey <rramsey1115@gmail.com>
2025-04-04 15:21:46 -0500

    added placeholder to Home view

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,3 +1,23 @@
 export const Home = () => {
-    return(<h1>Home</h1>)
+    return (<>
+        <header className="header">
+            <h1 className="header-heading">Home</h1>
+        </header>
+        <section className="section">
+            <article className="article">
+                <div className="article-div">
+                    <h4 className="article-heading">Intro</h4>
+                    <p>Talk about the project</p>
+                    <p>Talk about the project</p>
+                    <p>Talk about the project</p>
+                </div>
+                <div className="article-div">
+                    <h4 className="article-heading">Tech Used</h4>
+                    <p>Talk about the project</p>
+                    <p>Talk about the project</p>
+                    <p>Talk about the project</p>
+                </div>
+            </article>
+        </section>
+    </>)
 }
