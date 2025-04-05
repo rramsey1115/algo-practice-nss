f9dead0316e0681a9caf660ae49378c899fbd396
Ryan Ramsey <rramsey1115@gmail.com>
2025-04-05 15:41:03 -0500

    navbar style updates

--- src/Navbar/NavBar.jsx
+++ src/Navbar/NavBar.jsx
@@ -49,22 +49,30 @@ export const NavBar = () => {
   };
 
   return (
-    <Box sx={{
-      width: '100vw',
-      minWidth: "380px",
-      bgcolor: "#212121",
-      padding: "6px 0px",
-      position: 'fixed',
-      top: 0,
-      left: 0,
-      boxSizing: "border-box",
-      borderBottom: "1px solid whitesmoke"
-    }}>
+    <Box
+      sx={{
+        width: {
+          xs: '100vw', // full width on mobile
+          md: '85vw',  // 90% width on small screens and up
+        },
+        minWidth: '330px',
+        bgcolor: '#212121',
+        padding: "4px",
+        position: 'fixed',
+        top: 0,
+        left: {
+          xs: 0,
+          md: '5vw',
+        },
+        boxSizing: 'border-box',
+        borderBottom: '1px solid whitesmoke',
+      }}
+    >
       <Tabs
         value={value}
         onChange={handleChange}
         variant='scrollable'
-        scrollButtons
+        scrollButtons={true}
         allowScrollButtonsMobile
         aria-label="nav tabs"
         role="navigation"
@@ -72,14 +80,17 @@ export const NavBar = () => {
         textColor='whitesmoke'
         sx={{
           '& .MuiTab-root': {
-            color: 'inherit', 
+            color: 'inherit',
             fontFamily: 'inherit',
             textTransform: 'none',
             fontSize: "1rem",
             fontWeight: 400,
             '&:hover': {
-              color: (theme) => theme.palette.success.light, 
+              color: (theme) => theme.palette.success.light,
             },
+          },
+          '& .Mui-scroller': {
+            width: "80vw"
           },
           '& .Mui-selected': {
             color: (theme) => theme.palette.success.light,
